012c5d19fdda4359cab88ce2eb309ede109abef3
시온백 <tldhs05222@naver.com>
2022-10-26 21:39:37 +0900

    ✨ feat : 로그인 기능, 로그아웃 구현

---
+++ Server/src/main/java/ver37/Server/repository/JwtRepository.java
@@ -0,0 +1,23 @@
+package ver37.Server.repository;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import ver37.Server.entity.Jwt;
+
+import java.util.Optional;
+
+public interface JwtRepository extends JpaRepository<Jwt, Long> {
+    @Query("select t from Jwt t where t.refreshToken = :token")
+    Optional<Jwt> findRefreshToken(@Param("token") String token);
+
+    @Query("select t from Jwt t left join t.member m where t.member.memberId = :memberId")
+    Optional<Jwt> findMemberId(@Param("memberId") Long memberId);
+
+    @Query("delete from Jwt t where t.member.memberId = : memberId")
+    void deleteMemberId(@Param("memberId") Long memberId);
+
+    @Query("delete  from  Jwt t where t.refreshToken = : token")
+    void deleteToken(@Param("token") String token);
+}
