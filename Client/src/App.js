f58f6b1f9cae910022c22df0a37bb59983afbc91
haileyport <sugar9935@gmail.com>
2022-10-26 09:48:44 +0900

    📝 docs : 페이지 테스트 주석처리

--- Client/src/App.js
+++ Client/src/App.js
@@ -2,11 +2,7 @@ import React from 'react';
 import Details from './components/Detail';
 
 function App() {
-  return (
-    <div>
-      <Details />
-    </div>
-  );
+  return <div>{/* <Details /> */}</div>;
 }
 
 export default App;
