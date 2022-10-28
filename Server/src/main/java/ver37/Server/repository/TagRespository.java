91a132755ffbea0cb59b24fab18616d6281af85f
시온백 <tldhs05222@naver.com>
2022-10-28 15:05:59 +0900

    ♻️ refactor : 안쓰는 자바 파일 주석 처리 (추후 결과보고 삭제 예정)

--- Server/src/main/java/ver37/Server/repository/TagRespository.java
+++ Server/src/main/java/ver37/Server/repository/TagRespository.java
@@ -1,8 +1,8 @@
-package ver37.Server.repository;
-
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ver37.Server.entity.Tag;
-
-public interface TagRespository extends JpaRepository<Tag, Long> {
-}
+//package ver37.Server.repository;
+//
+//
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import ver37.Server.entity.Tag;
+//
+//public interface TagRespository extends JpaRepository<Tag, Long> {
+//}
