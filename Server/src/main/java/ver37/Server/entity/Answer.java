d3694b74a207887418b95615a8dd00c76f269f1b
시온백 <tldhs05222@naver.com>
2022-10-28 16:28:14 +0900

    ✨ feat : Answer 답변 등록 API 완성했습니다.

--- Server/src/main/java/ver37/Server/entity/Answer.java
+++ Server/src/main/java/ver37/Server/entity/Answer.java
@@ -1,7 +1,6 @@
 package ver37.Server.entity;
 
 import lombok.AccessLevel;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -15,27 +14,41 @@ import javax.validation.constraints.Size;
 public class Answer extends Auditing {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long answerId;
+    private Long answerId;
 
+    private Long checkingPostId;
 
     @Column(nullable = false, columnDefinition = "TEXT")
     @Size(min = 15)
     private String answerBody;
 
     @Column(nullable = false)
-    private Integer answerVote = 0;
+    private Integer likeCount = 0;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "member_id")
+    @JoinColumn(name = "MEMBER_ID")
     private Member member;
 
+
     @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "POST_ID")
     private Post post;
 
-    public Answer(String answerBody) {
+    public void addMember(Member member) {
+        this.member = member;
+    }
+
+    public void addPost(Post post) {
+        this.post = post;
+    }
+
+    public Answer(Long checkingPostId, String answerBody) {
+        this.checkingPostId = checkingPostId;
         this.answerBody = answerBody;
     }
 
+
     public void updateAnswerBody(String answerBody) {
+        this.answerBody = answerBody;
     }
 }
--- Server/src/main/java/ver37/Server/mapper/AnswerMapper.java
+++ Server/src/main/java/ver37/Server/mapper/AnswerMapper.java
@@ -1,15 +1,22 @@
 package ver37.Server.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
 import ver37.Server.dto.AnswerDto;
 import ver37.Server.entity.Answer;
 
-import java.util.List;
-
-@Mapper(componentModel =  "spring")
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface AnswerMapper {
-    Answer answerPostToAnswer(AnswerDto.Post requestBody);
-    Answer answerPatchToAnswer(AnswerDto.Patch requestBody);
-    AnswerDto.Response answerToResponse(Answer answer);
-    List<AnswerDto.Response> answersToAnswerResponses(List<Answer> answers);
+    default Answer answerPostDtoToAnswer(AnswerDto.Post post) {
+        Answer answer = new Answer(post.getPostId(), post.getAnswerBody());
+        return answer;
+    }
+
+    //    Answer answerPatchToAnswer(AnswerDto.Patch requestBody);
+    default AnswerDto.Response answerToResponse(Answer answer) {
+        AnswerDto.Response response = new AnswerDto.Response(answer);
+        return response;
+
+//    List<AnswerDto.Response> answersToAnswerResponses(List<Answer> answers);
+    }
 }
