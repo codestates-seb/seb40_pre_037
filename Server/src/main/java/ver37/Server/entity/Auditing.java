80721f0b3f61d74000d44804b90b600e6b798b8c
시온백 <tldhs05222@naver.com>
2022-10-28 18:17:50 +0900

    ♻️ refactor : Auditing.java 클래스 기능 완료

--- Server/src/main/java/ver37/Server/entity/Auditing.java
+++ Server/src/main/java/ver37/Server/entity/Auditing.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.Column;
@@ -20,13 +21,7 @@ public class Auditing {
     @Column(updatable = false)
     private LocalDateTime createdDate;
 
-    @LastModifiedBy
+    @LastModifiedDate
     private LocalDateTime lastModifiedDate;
 
-    @CreatedBy
-    @Column(updatable = false)
-    private String createdBy;
-
-    @LastModifiedBy
-    private String lastModifiedBy;
 }
