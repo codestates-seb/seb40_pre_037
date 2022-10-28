9de6ffd84f067fa34f01f87e645f0205a4c05f39
시온백 <tldhs05222@naver.com>
2022-10-29 02:21:35 +0900

    ♻️ refactor : 삭제할 파일들

--- Server/src/main/java/ver37/Server/repository/PostTagRepository.java
+++
@@ -1,7 +0,0 @@
-//package ver37.Server.repository;
-//
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import ver37.Server.entity.PostTag;
-//
-//public interface PostTagRepository extends JpaRepository<PostTag,Long> {
-//}
