5fb22c0bc8e0bccc0063c06c5e5d530667e7c9cc
시온백 <tldhs05222@naver.com>
2022-10-28 23:47:40 +0900

    ♻️ refactor 메소드명 변경

--- Server/src/main/java/ver37/Server/Service/PostService.java
+++ Server/src/main/java/ver37/Server/Service/PostService.java
@@ -2,16 +2,12 @@ package ver37.Server.Service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import ver37.Server.entity.Jwt;
 import ver37.Server.entity.Member;
 import ver37.Server.entity.Post;
 import ver37.Server.repository.JwtRepository;
-import ver37.Server.repository.MemberRepository;
 import ver37.Server.repository.PostRepository;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -24,14 +20,14 @@ public class PostService {
     //String jwtToken = request.getHeader("Authorization").replace("Bearer ", "");
 
     public Post creatPost(Post post) { //컨트롤러에서는 헤더와 post디티오를 받아와야함
-        Member member = getMemberByRequest();
+        Member member = getMemberByAccess();
         post.addMember(member);
 
         //토큰 가져오기 확인
         return postRepository.save(post);
     }
 
-    private Member getMemberByRequest() {
+    private Member getMemberByAccess() {
         String replace = request.getHeader("Authorization").replace("Bearer ", "");
         Member member = memberService.getMemberFromToken(replace);
         return member;
@@ -39,7 +35,7 @@ public class PostService {
 
     public Post patchPost(Post post) {
         Post verifyPost = findVerifyPost(post.getPostId());
-        Member memberByRequest = getMemberByRequest();
+        Member memberByRequest = getMemberByAccess();
         if (verifyPost.equals(memberByRequest)) {
             throw new RuntimeException("본인이 작성한 글만 수정가능합니다.");
         }
@@ -48,7 +44,6 @@ public class PostService {
         return verifyPost;
     }
 
-    //타이틀 바디 태그
     public Post findVerifyPost(Long postId) {
         return postRepository.findPostId(postId).orElseThrow(() -> new RuntimeException("존재하지 않는 게시글입니다."));
     }
