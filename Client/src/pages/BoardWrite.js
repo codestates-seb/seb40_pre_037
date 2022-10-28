1184bbda4702273967732dfbbb71a74b80d38dce
z1zonemoi <zizonemoi@gmail.com>
2022-10-28 14:19:30 +0900

    ✨ feat : 글작성 페이지 경로, UI 수정

--- Client/src/pages/BoardWrite.js
+++ Client/src/pages/BoardWrite.js
@@ -1,15 +1,29 @@
+/*
+책임개발자 : 최유정
+최초생성일 : 2022.10.27
+최근수정일 : 2022.10.28
+개요 :
+- components 경로 수정
+- 가로스크롤 방지로 UI 개선
+*/
+
 import React from 'react';
-import Nav from '../Components/Nav';
-import Ask from '../Components/Ask';
-import Footer from '../Components/Footer';
+import styled from 'styled-components';
+import Nav from '../components/Nav';
+import Ask from '../components/Ask';
+import Footer from '../components/Footer';
+
+const WriteContainer = styled.div`
+  overflow-x: hidden;
+`;
 
 function BoardWrite() {
   return (
-    <div>
+    <WriteContainer>
       <Nav />
       <Ask />
       <Footer />
-    </div>
+    </WriteContainer>
   );
 }
 
